ae5d6878c35a570ecb0af2f7b4ef48fde47ae528
steve <ustinsteve@gmail.com>
2025-11-23 22:57:22 +0100

    practiced django framework

---
+++ my_project/my_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import MyModel
+from .serializers import MyModelSerializer
+
+# Create your views here.
+
+
+class BookListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
